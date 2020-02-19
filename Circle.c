8a9293221d85152cde32d00fd2e67ff90b8eb05a
Prad250100 <pradyumna.aky@gmail.com>
2020-02-19 23:00:05 +0530

    new

---
+++ Circle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+ main(){
+float a,pi=3.14;
+int r;
+printf("Enter the radius of the circle : ");
+scanf("%d",&r);
+a=pi*r*r;
+printf("The area of the circle is : ");
+printf("%f",a);
+
+}
