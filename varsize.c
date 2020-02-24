fd81ae6fa73fd18a3269b5f90b98ccf1233e3a4f
Prad250100 <pradyumna.aky@gmail.com>
2020-02-25 00:05:04 +0530

    new

---
+++ varsize.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main(){
+int a;
+short b;
+char c;
+float d;
+double e;
+printf("Size of int is : %d\nSize of short is : %d\nSize of char is : %d\nSize of float is : %d\nSize of double is : %d\n",sizeof(a),sizeof(b),sizeof(c),sizeof(d),sizeof(e));
+
+
+}
+
